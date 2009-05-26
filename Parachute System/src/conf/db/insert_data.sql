7392e94ab401aa38a5f2413838a070b4001d095f
Lancer-Matrix <cewei@users.noreply.github.com>
2009-05-26 04:02:40 +0000

    Lunch. The overall view thing is now done, with the SQL code out.

--- Parachute System/src/conf/db/insert_data.sql
+++ Parachute System/src/conf/db/insert_data.sql
@@ -3,9 +3,15 @@ INSERT INTO para_type (type_prefix, name, life_span, max_jump, repack_cycle) VAL
 INSERT INTO para_type (type_prefix, name, life_span, max_jump, repack_cycle) VALUES('CRC', 'CRC One', 3, 33, 333);
 INSERT INTO para_type (type_prefix, name, life_span, max_jump, repack_cycle) VALUES('DRC', 'DRC One', 4, 44, 444);
 
-INSERT INTO parachute_system.para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(1, 'A', 'A1', '2009-04-12 00:00:00.0', 0, '');
-INSERT INTO parachute_system.para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(2, 'B', 'B2', '2009-04-12 00:00:00.0', 0, '');
-INSERT INTO parachute_system.para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(3, 'C', 'C3', '2009-04-12 00:00:00.0', 0, '');
-INSERT INTO parachute_system.para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(4, 'D', 'D4', '2009-04-12 00:00:00.0', 0, '');
+INSERT INTO para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(1, '0001', '1000', '2009-04-12', 0, '');
+INSERT INTO para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(1, '0002', '1001', '2009-04-12', 0, '');
+INSERT INTO para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(2, '0001', '2000', '2009-04-12', 0, '');
+INSERT INTO para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(3, '0001', '3000', '2009-04-12', 0, '');
+INSERT INTO para_inventory (type_prefix_no, chute_no, serial_no, date_of_mfg, no_of_jumps, status) VALUES(4, '0001', '4000', '2009-04-12', 0, '');
 
-INSERT INTO para_packing (type_prefix_no, chute_no, serial_no, date_packed, pack_by, inspect_by, check_type) VALUES (1, 'A', 'A1', '2009-04-12 00:00:00.0', 'packed', 'inspected', 'repacking');
+INSERT INTO para_packing (type_prefix_no, chute_no, serial_no, date_packed, pack_by, inspect_by, check_type) VALUES (1, '0001', '1000', '2009-04-12', 'packed', 'inspected', 'inspection');
+INSERT INTO para_packing (type_prefix_no, chute_no, serial_no, date_packed, pack_by, inspect_by, check_type) VALUES (1, '0001', '1000', '2009-10-15', 'packed', 'inspected', 'repacking');
+INSERT INTO para_packing (type_prefix_no, chute_no, serial_no, date_packed, pack_by, inspect_by, check_type) VALUES (1, '0002', '1001', '2009-04-12', 'packed', 'inspected', 'inspection');
+INSERT INTO para_packing (type_prefix_no, chute_no, serial_no, date_packed, pack_by, inspect_by, check_type) VALUES (2, '0001', '2000', '2009-04-12', 'packed', 'inspected', 'inspection');
+INSERT INTO para_packing (type_prefix_no, chute_no, serial_no, date_packed, pack_by, inspect_by, check_type) VALUES (3, '0001', '3000', '2009-04-12', 'packed', 'inspected', 'inspection');
+INSERT INTO para_packing (type_prefix_no, chute_no, serial_no, date_packed, pack_by, inspect_by, check_type) VALUES (4, '0001', '4000', '2009-04-12', 'packed', 'inspected', 'inspection');
